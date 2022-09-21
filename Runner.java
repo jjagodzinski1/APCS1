ebc7a77fd48e20b903c71c04d37aaac9458cc19f
jjagodzinski1 <113376251+jjagodzinski1@users.noreply.github.com>
2022-09-21 06:56:58 -0500

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,7 +1,7 @@
 public class Runner{
   public static void main(String[] args){
   //This is a test of the system.
-  System.out.println("Hello World");
+  System.out.println("I am currently testing things out.");
   
   }
 }
